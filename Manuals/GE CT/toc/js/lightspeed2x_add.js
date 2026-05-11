a1503ac4768222bb9b6122b1c6510bb0bf9b40c1
mikejackson-stack <mike.jackson@amtimagingsolutions.com>
2026-05-11 18:24:20 -0400

    Add GE CT: toc

---
+++ Manuals/GE CT/toc/js/lightspeed2x_add.js
@@ -0,0 +1,27 @@
+﻿
+ 
+ var dropdown = [
+["LightSpeed 2.X Pre-Installation Manual (GoldSeal)","../../ls_2.x/5266383.pdf"],
+["LightSpeed 2.X Installation Manual (GoldSeal)","../../ls_2.x/5266553.pdf"],
+["CT Cardiac Gating Option Manual","../../ls_2.x/2380526.pdf"],
+["Advantage 4D - Respiratory Gating","../../pdf/2380690.pdf"],
+["CRT &amp; LCD Remote Monitor &amp; Monitor-in-Room Boom Option","../../pdf/2390072.pdf"],
+["DASM I &amp; II - Installation Instructions","../../ls_2.x/2291842.pdf"],
+["LightSpeed and BrightSpeed DICOM Conformance Statement ","../../pdf/5198351.pdf"],
+["LightSpeed* Conformance Statement","../../pdf/5168904.pdf"],
+["LightSpeed Family Detector Change Procedure ","../../ls_2.x/2335850.pdf"],
+["LightSpeed Quick Tube Change Procedure","../../ls_2.x/2394243.pdf"],
+["LightSpeed Series Options Installation","../../pdf/2211224.pdf"],
+["LS Gantry Pushbutton and Display Flash","../../pdf/2385487.pdf"],
+["Octane1 1.0GB Memory Upgrade","../../ls_2.x/2373637.pdf"],
+["Prospective Gating - LightSpeed Plus","../../ls_2.x/2288566.pdf"],
+["SGI Octane 2 FRU, Replacement Instr.","../../ls_2.x/2300338.pdf"],
+["SmartScore Option Manual for CT/i &amp; LightSpeed","../../pdf/2224901.pdf"],
+["SmartStep Option Manual for LightSpeed","../../pdf/2345629.pdf"],
+["SnapShot Segment Image Quality","../../ls_2.x/2301701.pdf"],
+["UPS Installation: HSA &amp; LS 1.X - 4.X","../../pdf/2301052.pdf"],
+["2-Phase UPS Installation","../../pdf/5174051.pdf"] 
+];
+var dropdown_label = "";
+var bottom_text = "";
+
